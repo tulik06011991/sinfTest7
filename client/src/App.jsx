86ad3d27e4a1f712b09c205669c44c09d02d9ce6
realProject <tolqinmirsaliyev@gmail.com>
2024-10-22 12:35:07 +0530

    dsds

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import UploadQuiz from './components/faylYuklash'; // UploadQuiz komponentini import qilish
-import AdminFan from './components/AdminFan'
+import AdminFan from './components/AdminFan'; // AdminFan komponentini import qilish
+import Login from './components/Login'; // Login komponentini import qilish
+import Register from './components/Register'; // Register komponentini import qilish
+import Navbar from './components/Navbar'; // Navbar komponentini import qilish
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav className="bg-gray-800 p-4">
-          <ul className="flex space-x-4">
-            <li>
-              <Link to="/adminFan" className="text-white">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/upload" className="text-white">
-                Quiz Fayl Yuklash
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        <Navbar /> {/* Navbar komponentini qo'shamiz */}
 
         <div className="content">
           <Routes>
             <Route path="/" element={<h1 className="text-center text-3xl mt-10">Bosh sahifa</h1>} />
-            <Route path="/upload" element={<UploadQuiz />} /> {/* Upload sahifasi */}
-            <Route path="/adminFan" element={<AdminFan />} /> {/* Upload sahifasi */}
+            <Route path="/upload" element={<UploadQuiz />} /> {/* Fayl yuklash sahifasi */}
+            <Route path="/adminFan" element={<AdminFan />} /> {/* Admin fan sahifasi */}
+            <Route path="/login" element={<Login />} /> {/* Login sahifasi */}
+            <Route path="/register" element={<Register />} /> {/* Register sahifasi */}
           </Routes>
         </div>
       </div>
