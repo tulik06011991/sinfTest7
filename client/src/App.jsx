18da6b7da4e09eb268508b97c241f0afe976fba2
tulik <tolqinmirsaliyev@gmail.com>
2024-10-22 11:39:27 -0500

    sasasa

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import UploadQuiz from './components/faylYuklash'; // UploadQuiz komponentini import qilish
 import AdminFan from './components/AdminFan'; // AdminFan komponentini import qilish
 import Login from './components/Login'; // Login komponentini import qilish
 import Register from './components/Register'; // Register komponentini import qilish
 import Navbar from './components/Navbar'; // Navbar komponentini import qilish
-import Main from './components/Main'
-import Test  from './components/Test'
-import Admin from './components/Admins/Admins'
+import Main from './components/Main';
+import Test from './components/Test';
+import Admin from './components/Admins/Admins';
 import './App.css';
 
 function App() {
@@ -18,13 +18,16 @@ function App() {
 
         <div className="content">
           <Routes>
-            <Route path="/" element={<Main/>} />
+            <Route path="/" element={<Main />} />
             <Route path="/upload" element={<UploadQuiz />} /> {/* Fayl yuklash sahifasi */}
             <Route path="/adminFan" element={<AdminFan />} /> {/* Admin fan sahifasi */}
             <Route path="/login" element={<Login />} /> {/* Login sahifasi */}
             <Route path="/register" element={<Register />} />
-            <Route path="/test" element={<Test/>} />
-            <Route path="/admins" element={<Admin/>} />  {/* Register sahifasi */}
+            <Route path="/test" element={<Test />} />
+            <Route path="/admins" element={<Admin />} /> {/* Admin sahifasi */}
+
+            {/* Mavjud bo'lmagan yo'lga kirilganda login sahifasiga yo'naltirish */}
+            <Route path="*" element={<Navigate to="/login" replace />} />
           </Routes>
         </div>
       </div>
